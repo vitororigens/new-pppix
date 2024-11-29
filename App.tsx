8cf1fa7dfaeaf0fe460991d37501e7086de35436
vitororigens <vitorabreufjb@hotmail.com>
2024-11-28 21:57:56 -0300

    add file Routes

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import { ToastProvider } from 'react-native-toast-notifications';
 
 import theme from './src/theme';
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthRoutes } from "./src/routes/AuthRoutes";
+import { Routes } from "./src/routes/Routes";
 
 export default function App() {
   const [fontLoader] = useFonts({
@@ -24,7 +24,7 @@ export default function App() {
     <NavigationContainer>
       <ThemeProvider theme={theme}>
         <ToastProvider>
-          {fontLoader ? <AuthRoutes /> : <Loader />}
+          {fontLoader ? <Routes /> : <Loader />}
           <StatusBar style="auto" />
         </ToastProvider>
       </ThemeProvider>
